6e16d580994142a5e1e08dfa9abb65b09999e104
Muhammad Lawal <abbaraees@gmail.com>
2022-11-30 19:03:11 +0100

    feat: add 'add course' function
    Allow teacher to add course from the teacher's dashboard by sending a post
    request to the backend with the course name and description.

---
+++ app/static/scripts/courses.js
@@ -0,0 +1,36 @@
+document.addEventListener('click', (e)=> {
+    if (e.target.id == 'show-form-btn') {
+        document.getElementById("add-course-form").style.display = 'block'
+    }
+    else if (e.target.id == 'add-course-btn') {
+        addCourse()
+    }
+    else if (e.target.id == 'close-btn') {
+        document.getElementById("add-course-form").style.display = 'none'
+    }
+})
+
+function addCourse() {
+    const formData = new FormData(document.getElementById("add-course-form"))
+    fetch('/teachers/add_course', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "csrf_token": formData.get('csrf_token'),
+            "name": formData.get("name"),
+            "description": formData.get("description")
+        })
+    }).then((resp) => {
+        resp.json().then((data) => {
+            document.getElementById("courses").innerHTML += 
+                `<li>${data.course['name']}</li>`
+            document.getElementById("add-course-form").style.display = 'none'
+            document.getElementById("name").value = ""
+            document.getElementById("description").value = ""
+        }).catch((err)=> {
+            console.log(err)
+        })
+    })
+}
--- app/teachers/routes.py
+++ app/teachers/routes.py
@@ -1,9 +1,60 @@
+from flask import (
+    render_template, redirect, url_for, g, request, flash,jsonify
+)
+from werkzeug.datastructures import ImmutableMultiDict
+
 from app.auth.utils import require_role
 from app.teachers import bp
+from app.teachers.forms import AddCourseForm
+from app.models import Course, db
 
 
 
 @bp.route('/')
 @require_role('teacher')
 def index():
-    return '<h1>Welcome Teacher</h1>'
+    return render_template('teachers/index.html')
+
+
+@bp.route('/courses')
+@require_role('teacher')
+def courses():
+    courses = g.user.courses
+    form=AddCourseForm()
+
+    return render_template('teachers/courses.html', courses=courses, form=form)
+
+
+@bp.route('/add_course', methods=['POST'])
+@require_role('teacher')
+def add_course():
+    formdata = ImmutableMultiDict(request.get_json())
+    form = AddCourseForm(formdata)
+    if form.validate_on_submit():
+        course = Course(
+            name=form.name.data,
+            description=form.description.data
+        )
+        course.teacher = g.user
+        try:
+            db.session.add(course)
+            db.session.commit()
+
+            return {
+                'status': 'success',
+                'message': 'course added successfully',
+                'course': {
+                    'name': course.name,
+                    'description': course.description
+                }
+            }
+            
+        except:
+            db.session.rollback()
+
+
+    return jsonify({
+        'status': 'failed',
+        'message': 'Failed to add course'
+    }), 400
+
