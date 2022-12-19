e5a69daf53707137eb13e10c45091e32f712d523
Muhammad Lawal <abbaraees@gmail.com>
2022-12-19 09:18:15 +0100

    feat: Add view course feature for students
    This commit added a feature to allow students to view the content of a course
    and list all courses they are enrolled in. This should help improve the user
    experience when navigating the platform.

--- app/students/routes.py
+++ app/students/routes.py
@@ -3,9 +3,20 @@ from flask import render_template, url_for, redirect
 
 from app.auth.utils import require_role
 from app.students import bp
+from app.models import Course
 
 
 @bp.route('/dashboard')
 @require_role('student')
 def dashboard():
-    return render_template('students/dashboard.html')
+    courses = Course.query.filter_by(published=True)
+    return render_template('students/dashboard.html', courses=courses)
+
+
+@bp.route('/learn/course/<int:id>')
+@require_role('student')
+def view_course(id):
+    course = Course.query.filter_by(id=id, published=True).first_or_404()
+
+    return render_template('students/view_course.html', course=course, enumerate=enumerate)
+
